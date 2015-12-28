63dddd8982d570baea8244b58e5467a77dfc52f4
tikismoke <webmaster@tikijs.dyndns.org>
2015-12-28 22:29:58 +0100

    looking for xpl to be intercept

--- lib/nabaztag.py
+++ lib/nabaztag.py
@@ -1,112 +1,121 @@
 # !/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-#import PyNUT
+# import PyNUT
 import time
 from domogik_packages.plugin_nabaztag.lib.nabaztag_client import NabaztagClient, getClientId
 from domogik_packages.plugin_nabaztag.lib.client_devices import GetDeviceParams, OPERATORS_SERVICE
+
 
 class NabaztagClientsManagerException(Exception):
     """
     Nabaztag Manager exception
     """
+
     def __init__(self, value):
         Exception.__init__(self)
         self.value = "NabaztagClientsManager exception" + value
 
     def __str__(self):
         return repr(self.value)
-        
-class NabaztagClientsManager :
+
+
+class NabaztagClientsManager:
     """
     Manager Nabaztag Web Clients.
     """
-    def __init__ (self, xplPlugin, cb_send_xPL) :
+
+    def __init__(self, xplPlugin, cb_send_xPL):
         """Init manager Nabaztag Clients"""
         self._xplPlugin = xplPlugin
         self._cb_send_xPL = cb_send_xPL
-        self.clients = {} # list of all Nabaztag Clients
+        self.clients = {}  # list of all Nabaztag Clients
         self._xplPlugin.log.info(u"Manager Nabaztag Clients is ready.")
-        
+
     def _del__(self):
         """Delete all Nabaztag CLients"""
         print "try __del__ NabaztagClients"
-        for id in self.clients : self.clients[id] = None
-        
+        for id in self.clients: self.clients[id] = None
+
     def stop(self):
         """Close all Nabaztag CLients"""
         self._xplPlugin.log.info(u"Closing NabaztagManager.")
-        for id in self.clients : self.clients[id].close()
-    
+        for id in self.clients: self.clients[id].close()
+
     def addClient(self, instance):
         """Add a Nabaztag from domogik instance"""
         name = getClientId(instance)
-        if self.clients.has_key(name) :
+        if self.clients.has_key(name):
             self._xplPlugin.log.debug(u"Manager Client : Nabaztag Client {0} already exist, not added.".format(name))
             return False
         else:
             params = GetDeviceParams(self._xplPlugin, instance)
-            if params :
-                if params['operator'] in OPERATORS_SERVICE :
-                    self.clients[name] = NabaztagClient(self,  params,  self._xplPlugin.log)
-                else :
-                    self._xplPlugin.log.error(u"Manager Client : Nabaztag Client type {0} not exist, not added.".format(name))
-                    return False                
+            if params:
+                if params['operator'] in OPERATORS_SERVICE:
+                    self.clients[name] = NabaztagClient(self, params, self._xplPlugin.log)
+                else:
+                    self._xplPlugin.log.error(
+                        u"Manager Client : Nabaztag Client type {0} not exist, not added.".format(name))
+                    return False
                 self._xplPlugin.log.info(u"Manager Client : created new client {0}.".format(name))
-            else : 
-                self._xplPlugin.log.info(u"Manager Client : instance not configured can't add new client {0}.".format(name))
+            else:
+                self._xplPlugin.log.info(
+                    u"Manager Client : instance not configured can't add new client {0}.".format(name))
                 return False
             return True
-        
+
     def removeClient(self, name):
         """Remove a Nabaztag client and close it"""
         client = self.getClient(name)
-        if client :
+        if client:
             client.close()
             self.clients.pop(name)
-            
+
     def getClient(self, id):
         """Get Nabaztag client object by id."""
-        if self.clients.has_key(id) :
+        if self.clients.has_key(id):
             return self.clients[id]
-        else : 
+        else:
             return None
-            
+
     def getIdsClient(self, idToCheck):
         """Get Nabaztag client key ids."""
-        retval =[]
+        retval = []
         findId = ""
-        self._xplPlugin.log.debug (u"getIdsClient check for device : {0}".format(idToCheck))
-        if isinstance(idToCheck,  NabaztagClient) :
-            for id in self.clients.keys() :
-                if self.clients[id] == idToCheck :
+        self._xplPlugin.log.debug(u"getIdsClient check for device : {0}".format(idToCheck))
+        if isinstance(idToCheck, NabaztagClient):
+            for id in self.clients.keys():
+                if self.clients[id] == idToCheck:
                     retval = [id]
                     break
-        else :
-            self._xplPlugin.log.debug (u"getIdsClient, no NabaztagClient instance...")
-            if isinstance(idToCheck,  str) :  
+        else:
+            self._xplPlugin.log.debug(u"getIdsClient, no NabaztagClient instance...")
+            if isinstance(idToCheck, str):
                 findId = idToCheck
-                self._xplPlugin.log.debug (u"str instance...")
-            else :
-                if isinstance(idToCheck,  dict) :
-                    if idToCheck.has_key('to') : findId = idToCheck['to']
-                    else :
-                        if idToCheck.has_key('name') and idToCheck.has_key('id'): 
+                self._xplPlugin.log.debug(u"str instance...")
+            else:
+                if isinstance(idToCheck, dict):
+                    if idToCheck.has_key('to'):
+                        findId = idToCheck['to']
+                    else:
+                        if idToCheck.has_key('name') and idToCheck.has_key('id'):
                             findId = getClientId(idToCheck)
-            if self.clients.has_key(findId) : 
+            if self.clients.has_key(findId):
                 retval = [findId]
-                self._xplPlugin.log.debug (u"key id type find")
-            else :
-                self._xplPlugin.log.debug (u"No key id type, search {0} in devices {1}".format(findId, self.clients.keys()))
-                for id in self.clients.keys() :
-                    self._xplPlugin.log.debug(u"Search in list by device key : {0}".format(self.clients[id].domogikDevice))
-                    if self.clients[id].domogikDevice == findId : 
+                self._xplPlugin.log.debug(u"key id type find")
+            else:
+                self._xplPlugin.log.debug(
+                    u"No key id type, search {0} in devices {1}".format(findId, self.clients.keys()))
+                for id in self.clients.keys():
+                    self._xplPlugin.log.debug(
+                        u"Search in list by device key : {0}".format(self.clients[id].domogikDevice))
+                    if self.clients[id].domogikDevice == findId:
                         self._xplPlugin.log.debug('find Nabaztag Client :)')
                         retval.append(id)
         self._xplPlugin.log.debug(u"getIdsClient result : {0}".format(retval))
         return retval
-        
-    def refreshClientDevice(self,  client):
+
+    def refreshClientDevice(self, client):
         """Request a refresh domogik device data for a Nabaztag Client."""
         cli = MQSyncReq(zmq.Context())
         msg = MQMessage()
@@ -116,30 +125,31 @@ class NabaztagClientsManager :
         msg.add_data('host', get_sanitized_hostname())
         devices = cli.request('dbmgr', msg.get(), timeout=10).get()
         for a_device in devices:
-            if a_device['device_type_id'] == client._device['device_type_id']  and a_device['id'] == client._device['id'] :
-                if a_device['name'] != client.device['name'] : # rename and change key client id
+            if a_device['device_type_id'] == client._device['device_type_id'] and a_device['id'] == client._device[
+                'id']:
+                if a_device['name'] != client.device['name']:  # rename and change key client id
                     old_id = getClientId(client._device)
                     self.clients[getClientId(a_device)] = self.clients.pop(old_id)
-                    self._xplPlugin.log.info(u"Nabaztag Client {0} is rename {1}".format(old_id,  getClientId(a_device)))
+                    self._xplPlugin.log.info(u"Nabaztag Client {0} is rename {1}".format(old_id, getClientId(a_device)))
                 client.updateDevice(a_device)
                 break
 
     def NabaztagClientsConnection(self):
         """ Send a Notification connection Nabaztag to all Clients."""
-        for id in self.clients :
+        for id in self.clients:
             message = {'to': self.clients[id]._operator.to}
             message['header'] = self._xplPlugin.get_config("msg_header")
             message['title'] = 'Connexion'
             message['body'] = u"Your Terminal is registered to receive notification :)"
             data = self.clients[id]._operator.send(message)
-            if data['error'] == '' : del data['error']
+            if data['error'] == '': del data['error']
             data['to'] = self.clients[id].domogikDevice
             self.sendXplAck(data)
 
-    def sendXplAck(self,  data):
+    def sendXplAck(self, data):
         """Send an ack xpl message"""
         self._cb_send_xPL("sendmsg.confirm", data)
 
-    def sendXplTrig(self,  schema,  data):
+    def sendXplTrig(self, schema, data):
         """Send an xpl message"""
-        self._cb_send_xPL(schema,  data)
+        self._cb_send_xPL(schema, data)
